11a48a0a4f923064dce4d2584662c9f72a62f27a
Daniel Imal <mrdanimal@gmail.com>
2024-04-15 12:51:10 -0400

    cleanup

--- scaffold.js
+++ scaffold.js
@@ -1,16 +1,25 @@
 const fs = require('fs');
 const path = require('path');
 
+const excludedFiles = ['dist', 'node_modules', 'yarn.lock'];
+
+// Get directory name from command line argument
+const dirName = process.argv[2];
+
+const sourceDir = path.join(__dirname, 'sketches/base');
+const targetDir = path.join(__dirname, 'sketches', dirName);
+
+copyDirectory(sourceDir, targetDir);
+
+console.log(`Sketch './sketches/${dirName}' created.`);
+
 function copyDirectory(source, target) {
-  // Create target directory if it doesn't exist
   if (!fs.existsSync(target)) {
     fs.mkdirSync(target);
   }
 
-  // Get list of files and directories in source directory
   const files = fs.readdirSync(source);
 
-  // Iterate through each file/directory
   files.forEach((file) => {
     const sourcePath = path.join(source, file);
 
@@ -18,28 +27,12 @@ function copyDirectory(source, target) {
     let targetFileName = file.replace(/^base(\.html|\.ts)$/, `${dirName}$1`);
     const targetPath = path.join(target, targetFileName);
 
-    // Exclude dist, node_modules, and yarn.lock
-    if (file !== 'dist' && file !== 'node_modules' && file !== 'yarn.lock') {
-      // Check if it's a directory
+    if (!excludedFiles.includes(file)) {
       if (fs.statSync(sourcePath).isDirectory()) {
-        // Recursively copy directory
         copyDirectory(sourcePath, targetPath);
       } else {
-        // Copy file
         fs.copyFileSync(sourcePath, targetPath);
       }
     }
   });
 }
-
-// Get directory name from command line argument
-const dirName = process.argv[2];
-
-// Define source and target directories
-const sourceDir = path.join(__dirname, 'sketches/base');
-const targetDir = path.join(__dirname, 'sketches', dirName);
-
-// Copy directory
-copyDirectory(sourceDir, targetDir);
-
-console.log(`Directory '${dirName}' copied successfully.`);
