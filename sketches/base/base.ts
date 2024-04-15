ee285aa49a192cf5a4ab961a5058079947a86c48
Daniel Imal <mrdanimal@gmail.com>
2024-04-15 12:21:54 -0400

    cleanup

--- sketches/base/base.ts
+++ sketches/base/base.ts
@@ -1,10 +1,8 @@
 document.body.onload = () => {
-  console.log('loaded');
-
   const canvas = document.getElementById('canvas') as HTMLCanvasElement;
   const ctx = canvas.getContext('2d');
-  console.log({ canvas, ctx });
   const { width, height } = canvas;
+
   ctx.fillStyle = 'red';
   ctx.save();
   ctx.translate(width / 4, height / 4);
