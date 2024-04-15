81bc89b6b05fcefdb3f70978536081ed2d8b7068
Daniel Imal <mrdanimal@gmail.com>
2024-04-15 12:26:05 -0400

    extract draw func

--- sketches/base/base.ts
+++ sketches/base/base.ts
@@ -1,11 +1,15 @@
 document.body.onload = () => {
   const canvas = document.getElementById('canvas') as HTMLCanvasElement;
   const ctx = canvas.getContext('2d');
-  const { width, height } = canvas;
 
+  draw(ctx);
+};
+
+function draw(ctx: CanvasRenderingContext2D) {
+  const { width, height } = ctx.canvas;
   ctx.fillStyle = 'red';
   ctx.save();
   ctx.translate(width / 4, height / 4);
   ctx.fillRect(0, 0, width / 2, height / 2);
   ctx.restore();
-};
+}
