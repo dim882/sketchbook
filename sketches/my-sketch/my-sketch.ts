3fa6a4ac4b3804bde49d2de2f64d22c7e9080d9b
Daniel Imal <mrdanimal@gmail.com>
2024-04-16 11:19:43 -0400

    copy over fuzzer code with modifications

--- sketches/my-sketch/my-sketch.ts
+++ sketches/my-sketch/my-sketch.ts
@@ -1,3 +1,6 @@
+export type IRange = [lower: number, upper: number];
+export type I2DTuple = [number, number];
+
 document.body.onload = () => {
   const canvas = document.getElementById('canvas') as HTMLCanvasElement;
   const ctx = canvas.getContext('2d');
@@ -11,3 +14,52 @@ function render(ctx: CanvasRenderingContext2D) {
   ctx.fillStyle = '#000';
   ctx.fillRect(0, 0, width, height);
 }
+export type PseudoRandomNumberGenerator = () => number;
+
+// The returned fuzzer function:
+//  - Finds points random distance from the given point using a Gaussian distribution
+//  - Draws lines at those points with random lengths at random angles
+// prettier-ignore
+
+type MakeFuzzerArgs = {
+  context: CanvasRenderingContext2D;
+  radius: number;
+  iterations: number;
+  lengthRange?: IRange;
+}
+export function makeFuzzer({ context, radius, iterations, lengthRange = [0.4, 0.5] }: MakeFuzzerArgs) {
+  const [lengthLower, lengthHigher] = lengthRange;
+
+  return function makeFuzz(x: number, y: number, strokeStyle?: string) {
+    for (let i = 0; i < iterations; i++) {
+      const center: I2DTuple = [(Math.random() * radius) / 2, (Math.random() * radius) / 2];
+      const length = getInteger(Math.random, radius * lengthLower, radius * lengthHigher);
+      const angle1 = getFloat(Math.random, 0, 2 * Math.PI);
+      const point1: I2DTuple = [center[0] + length * Math.cos(angle1), center[1] + length * Math.sin(angle1)];
+
+      context.save();
+      if (strokeStyle) {
+        context.strokeStyle = strokeStyle;
+      }
+
+      context.translate(x, y);
+      context.beginPath();
+      context.moveTo(...center);
+      context.lineTo(...point1);
+      context.stroke();
+      context.restore();
+    }
+  };
+}
+
+export const getFloat = (generateNumber: PseudoRandomNumberGenerator, lower: number = 0, upper: number = 1) => {
+  return (upper - lower) * generateNumber() + lower;
+};
+
+export const getInteger = (generateNumber: PseudoRandomNumberGenerator, lower: number = 0, upper: number = 1) => {
+  return Math.floor(getFloat(generateNumber, lower, upper));
+};
+
+export const getBoolean = (generateNumber: PseudoRandomNumberGenerator, probabilityTrue: number = 0.5) => {
+  return generateNumber() < probabilityTrue;
+};
