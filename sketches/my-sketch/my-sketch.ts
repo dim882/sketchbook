5951d2ab5ea34f48cbb3c1accc911967d5e05c91
Daniel Imal <mrdanimal@gmail.com>
2024-04-16 11:06:22 -0400

    center a div

--- sketches/my-sketch/my-sketch.ts
+++ sketches/my-sketch/my-sketch.ts
@@ -8,9 +8,6 @@ document.body.onload = () => {
 function render(ctx: CanvasRenderingContext2D) {
   const { width, height } = ctx.canvas;
 
-  ctx.fillStyle = 'red';
-  ctx.save();
-  ctx.translate(width / 4, height / 4);
-  ctx.fillRect(0, 0, width / 2, height / 2);
-  ctx.restore();
+  ctx.fillStyle = '#000';
+  ctx.fillRect(0, 0, width, height);
 }
