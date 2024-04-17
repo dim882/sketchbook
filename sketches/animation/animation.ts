9279d29a2aad4c1dc5473631055c01f727c715ca
Daniel Imal <mrdanimal@gmail.com>
2024-04-17 17:40:46 -0400

    try a simple animation. Im too tired

--- sketches/animation/animation.ts
+++ sketches/animation/animation.ts
@@ -7,10 +7,34 @@ document.body.onload = () => {
   const canvas = document.getElementById('canvas') as HTMLCanvasElement;
   const context = canvas.getContext('2d');
 
-  render(context);
+  const fps = 60;
+  let frameDuration = 1000 / fps;
+  let lastFrameTime = 0;
+
+  let t = 0;
+  function animate(time: number) {
+    requestAnimationFrame(animate);
+    if (time - lastFrameTime < frameDuration) return;
+    lastFrameTime = time;
+
+    render(context, t); // Assuming `context` is accessible in this scope
+    t++;
+  }
+
+  requestAnimationFrame(animate);
 };
 
-function render(context: CanvasRenderingContext2D) {
+function render(context: CanvasRenderingContext2D, t: number) {
   const { width, height } = context.canvas;
   const center: IPointTuple = [width / 2, height / 2];
+
+  const radius = Math.abs(Math.sin(t) * 100);
+  console.log(t, radius);
+
+  context.fillStyle = '#fff';
+  context.fillRect(0, 0, width, height);
+
+  context.arc(...center, radius, 0, 2 * Math.PI);
+  context.fillStyle = 'red';
+  context.fill();
 }
--- sketches/animation/rollup.config.js
+++ sketches/animation/rollup.config.js
@@ -2,7 +2,7 @@ import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default {
-  input: 'blob-grid.ts',
+  input: 'animation.ts',
   output: {
     file: 'dist/bundle.js',
     format: 'module',
