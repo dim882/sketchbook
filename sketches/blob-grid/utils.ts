ff085cf4b0f3856aa8b0492cf5cf066a7db8ef4d
Daniel Imal <mrdanimal@gmail.com>
2024-04-17 09:36:22 -0400

    rename page titles

--- sketches/blob-grid/utils.ts
+++ sketches/blob-grid/utils.ts
@@ -2,40 +2,7 @@ export type PseudoRandomNumberGenerator = () => number;
 export type IRange = [lower: number, upper: number];
 export type I2DTuple = [number, number];
 
-export type MakeFuzzerArgs = {
-  context: CanvasRenderingContext2D;
-  radius: number;
-  iterations: number;
-  lengthRange?: IRange;
-};
-
-export function makeFuzzer({ context, radius, iterations, lengthRange = [0.4, 0.5] }: MakeFuzzerArgs) {
-  const [lengthLower, lengthHigher] = lengthRange;
-
-  // The returned fuzzer function:
-  //  - Finds points random distance from the given point using a Gaussian distribution
-  //  - Draws lines at those points with random lengths at random angles
-  return function makeFuzz(x: number, y: number, strokeStyle?: string) {
-    for (let i = 0; i < iterations; i++) {
-      const center: I2DTuple = [(Math.random() * radius) / 2, (Math.random() * radius) / 2];
-      const length = getInteger(Math.random, radius * lengthLower, radius * lengthHigher);
-      const angle1 = getFloat(Math.random, 0, 2 * Math.PI);
-      const point1: I2DTuple = [center[0] + length * Math.cos(angle1), center[1] + length * Math.sin(angle1)];
-
-      context.save();
-      if (strokeStyle) {
-        context.strokeStyle = strokeStyle;
-      }
-
-      context.translate(x, y);
-      context.beginPath();
-      context.moveTo(...center);
-      context.lineTo(...point1);
-      context.stroke();
-      context.restore();
-    }
-  };
-}
+export type IPointTuple = [number, number];
 
 export const getFloat = (generateNumber: PseudoRandomNumberGenerator, lower: number = 0, upper: number = 1) => {
   return (upper - lower) * generateNumber() + lower;
@@ -92,3 +59,15 @@ export const createPRNG = (seed: number): PRNG => {
 
   return prng;
 };
+
+export function tracePath(context: CanvasRenderingContext2D, points: IPointTuple[]) {
+  context.beginPath();
+  points.forEach(([x, y], index) => (index === 0 ? context.moveTo(x, y) : context.lineTo(x, y)));
+  context.closePath();
+}
+
+export function saveAndRestore(context: CanvasRenderingContext2D, callback: () => void) {
+  context.save();
+  callback();
+  context.restore();
+}
