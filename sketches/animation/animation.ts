360c9e9e2d16f48b60031a46594291ff20457e84
Daniel Imal <mrdanimal@gmail.com>
2024-04-17 18:37:30 -0400

    extract loop function

--- sketches/animation/animation.ts
+++ sketches/animation/animation.ts
@@ -1,4 +1,4 @@
-import { IPointTuple } from './utils.js';
+import { IPointTuple, loop } from './utils.js';
 
 // const prng = createPRNG(40502);
 const prng = Math.random;
@@ -7,30 +7,15 @@ document.body.onload = () => {
   const canvas = document.getElementById('canvas') as HTMLCanvasElement;
   const context = canvas.getContext('2d');
 
-  const fps = 60;
-  let frameDuration = 1000 / fps;
-  let lastFrameTime = 0;
-
-  let t = 0;
-  function animate(time: number) {
-    requestAnimationFrame(animate);
-
-    if (time - lastFrameTime < frameDuration) return;
-    lastFrameTime = time;
-
-    render(context, t); // Assuming `context` is accessible in this scope
-    t++;
-  }
-
-  requestAnimationFrame(animate);
+  loop(context, render, 60);
 };
 
 function render(context: CanvasRenderingContext2D, t: number) {
   const { width, height } = context.canvas;
   const center: IPointTuple = [width / 2, height / 2];
 
-  const radius = Math.floor(Math.abs(Math.sin(t * 0.01) * 100));
-  console.log(radius);
+  const radius = Math.floor(Math.abs(Math.sin(t * 0.05) * 100));
+
   context.clearRect(0, 0, width, height);
 
   context.fillStyle = '#fff';
