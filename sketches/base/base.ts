7547dc0d8d9cf20e4c517b11b4ff3e8d3c42836b
Daniel Imal <mrdanimal@gmail.com>
2024-04-24 17:09:03 -0400

    create new base sketch

--- sketches/base/base.ts
+++ sketches/base/base.ts
@@ -1,16 +1,23 @@
-document.body.onload = () => {
-  const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-  const ctx = canvas.getContext('2d');
+import { IPointTuple } from './base.utils';
 
-  render(ctx);
-};
+// const prng = createPRNG(40502);
+const prng = Math.random;
+
+window.addEventListener('DOMContentLoaded', () => {
+  const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+  const context = canvas.getContext('2d');
+
+  render(context);
+});
 
 function render(context: CanvasRenderingContext2D) {
   const { width, height } = context.canvas;
+  const center: IPointTuple = [width / 2, height / 2];
+
   //  Do work here
   context.fillStyle = 'red';
   context.save();
   context.translate(width / 4, height / 4);
-  context.fillRect(0, 0, width / 2, height / 2);
+  context.fillRect(0, 0, ...center);
   context.restore();
 }
