1a4c064bad94e8fb7a36955862389cdb60e4ace4
Daniel Imal <mrdanimal@gmail.com>
2024-04-24 17:38:11 -0400

    randomize lightness

--- sketches/arcs/arcs.ts
+++ sketches/arcs/arcs.ts
@@ -13,14 +13,18 @@ function render(context: CanvasRenderingContext2D) {
   const { width, height } = context.canvas;
   const center: IPointTuple = [width / 2, height / 2];
 
+  const baseHue = getInteger(prng, 0, 270);
+
   let radius = 50;
   while (radius < width / 2) {
     const FULL_ROTATION = 2 * Math.PI;
     const startAngle = getFloat(prng, 0, FULL_ROTATION);
     const endAngle = getFloat(prng, startAngle, FULL_ROTATION);
     const width = getFloat(prng, 10, 50);
+    const lightness = getInteger(prng, 10, 100);
+    const arcColor = `lch(${lightness}% 0% ${baseHue} / 1)`;
 
-    context.strokeStyle = '#000';
+    context.strokeStyle = arcColor;
     context.beginPath();
     context.arc(...center, radius, startAngle, endAngle);
     context.lineWidth = width;
