f75db702a0f9b4e4bfb1dc1b3bb7280ac3bacf9d
Daniel Imal <mrdanimal@gmail.com>
2024-05-03 13:00:15 -0400

    draw a trangle

--- sketches/radiation/radiation.ts
+++ sketches/radiation/radiation.ts
@@ -15,9 +15,28 @@ function render(context: CanvasRenderingContext2D) {
   const center: IPointTuple = [width / 2, height / 2];
 
   //  Do work here
-  context.fillStyle = 'red';
-  context.save();
-  context.translate(width / 4, height / 4);
-  context.fillRect(0, 0, ...center);
-  context.restore();
+  context.fillStyle = `#000`;
+  context.fillRect(0, 0, width, height);
+
+  context.translate(...center);
+  traceEquilateralTriangle(context, 0, 0, 400);
+  context.fillStyle = '#fff';
+  context.fill();
+}
+
+function traceEquilateralTriangle(context: CanvasRenderingContext2D, cx: number, cy: number, sideLength: number): void {
+  const height = (sideLength * Math.sqrt(3)) / 2;
+  const verticalOffset = (2 / 3) * height;
+
+  tracePath(context, [
+    [cx, cy - verticalOffset],
+    [cx + sideLength / 2, cy + (1 / 3) * height],
+    [cx - sideLength / 2, cy + (1 / 3) * height],
+  ]);
+}
+
+function tracePath(context: CanvasRenderingContext2D, points: IPointTuple[]) {
+  context.beginPath();
+  points.forEach(([x, y], index) => (index === 0 ? context.moveTo(x, y) : context.lineTo(x, y)));
+  context.closePath();
 }
--- sketches/radiation/rollup.config.js
+++ sketches/radiation/rollup.config.js
@@ -2,7 +2,7 @@ import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default {
-  input: 'base.ts',
+  input: 'radiation.ts',
   output: {
     file: 'dist/bundle.js',
     format: 'module',
