11f913bebff819aa59129765a34d4328301e2585
Daniel Imal <mrdanimal@gmail.com>
2024-05-07 16:06:17 -0400

    Revert web component install and rollup config change
    This reverts commit 777ed32c1629f9e473b1c8f8a0810bb3a23e69ef.

--- sketches/radiation/rollup.config.js
+++ sketches/radiation/rollup.config.js
@@ -1,12 +1,5 @@
-import fs from 'fs';
-import path from 'path';
 import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import html from '@rollup/plugin-html';
-
-const directory = path.dirname(new URL(import.meta.url).pathname);
-const templatePath = path.join(directory, 'radiation.html');
-const template = fs.readFileSync(templatePath, { encoding: 'utf8' });
 
 export default {
   input: 'radiation.ts',
@@ -15,16 +8,5 @@ export default {
     format: 'module',
     sourcemap: true,
   },
-  plugins: [
-    nodeResolve(),
-    typescript(),
-    html({
-      template: ({ attributes, files, meta, publicPath, title }) => {
-        const scripts = (files.js || [])
-          .map(({ fileName }) => `<script src="${publicPath}${fileName}"></script>`)
-          .join('\n');
-        return template.replace('${scripts}', scripts);
-      },
-    }),
-  ],
+  plugins: [nodeResolve(), typescript()],
 };
