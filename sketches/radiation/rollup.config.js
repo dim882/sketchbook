9017305dc122026152ca833d2097ee8021b160c7
Daniel Imal <mrdanimal@gmail.com>
2024-05-07 15:35:57 -0400

    set up html template

--- sketches/radiation/rollup.config.js
+++ sketches/radiation/rollup.config.js
@@ -1,5 +1,12 @@
+import fs from 'fs';
+import path from 'path';
 import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import html from '@rollup/plugin-html';
+
+const directory = path.dirname(new URL(import.meta.url).pathname);
+const templatePath = path.join(directory, 'radiation.html');
+const template = fs.readFileSync(templatePath, { encoding: 'utf8' });
 
 export default {
   input: 'radiation.ts',
@@ -8,5 +15,16 @@ export default {
     format: 'module',
     sourcemap: true,
   },
-  plugins: [nodeResolve(), typescript()],
+  plugins: [
+    nodeResolve(),
+    typescript(),
+    html({
+      template: ({ attributes, files, meta, publicPath, title }) => {
+        const scripts = (files.js || [])
+          .map(({ fileName }) => `<script src="${publicPath}${fileName}"></script>`)
+          .join('\n');
+        return template.replace('${scripts}', scripts);
+      },
+    }),
+  ],
 };
