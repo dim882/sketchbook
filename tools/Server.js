2005c1ff92a6dfaa7573e877cd3122961cee7586
Daniel Imal <mrdanimal@gmail.com>
2024-05-08 09:45:00 -0400

    add a server. its not complete yet

---
+++ tools/Server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Route to list all sketches
+app.get('/', (req, res) => {
+  const distPath = path.join(__dirname, '../sketches');
+  console.log({ distPath });
+  fs.readdir(distPath, { withFileTypes: true }, (err, files) => {
+    if (err) {
+      res.status(500).send('Failed to read sketches directory');
+      return;
+    }
+
+    const dirs = files.filter((file) => file.isDirectory()).map((dir) => dir.name);
+    let html = `
+      <h1>Sketches</h1>
+      <ul>
+        ${dirs.map((dir) => `<li><a href="/sketches/${dir}">${dir}</a></li>`).join('')}
+      </ul>
+      `;
+
+    res.send(html);
+  });
+});
+
+// Route to handle sketch requests
+app.get('/sketches/:sketchName', (req, res) => {
+  const sketchName = req.params.sketchName;
+  const filePath = path.join(__dirname, '../sketches', sketchName, `${sketchName}.html`);
+  console.log(filePath);
+
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      res.status(404).send('Sketch not found');
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
