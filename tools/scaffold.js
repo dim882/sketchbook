4a49eb0be4fbb3c76efa174d7139988a0a3b6477
Daniel Imal <mrdanimal@gmail.com>
2024-05-13 13:52:00 -0400

    rename stuff and add comments

--- tools/scaffold.js
+++ tools/scaffold.js
@@ -6,15 +6,15 @@ import { execSync } from 'child_process';
 
 const excludedFiles = ['dist', 'node_modules', 'yarn.lock'];
 
-const sourceDirName = process.argv[2];
-const targetDirName = process.argv[3];
+const sourceName = process.argv[2];
+const targetName = process.argv[3];
 
-const sourceDir = path.join(__dirname, '../sketches', sourceDirName);
-const targetDir = path.join(__dirname, '../sketches', targetDirName);
+const sourceDir = path.join(__dirname, '../sketches', sourceName);
+const targetDir = path.join(__dirname, '../sketches', targetName);
 
 copyDirectory(sourceDir, targetDir);
 
-console.log(`Sketch './sketches/${targetDirName}' created.`);
+console.log(`Sketch './sketches/${targetName}' created.`);
 
 function copyDirectory(source, target) {
   if (!fs.existsSync(target)) {
@@ -49,35 +49,34 @@ function copyDirectory(source, target) {
       try {
         let packageData = fs.readFileSync(packageJsonPath, 'utf8');
         let packageJson = JSON.parse(packageData);
-        packageJson.name = targetDirName; // Replace the `name` field with `targetDirName`
+        packageJson.name = targetName; // Replace the `name` field with `targetDirName`
         fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2)); // Write back to the same file with formatting
       } catch (error) {
         console.error('Error processing package.json:', error);
       }
     }
 
+    // Replace any instances of sourceDir with targetDir in rollup files
     if (file === 'rollup.config.js') {
       const rollupConfigPath = path.join(target, file);
 
       try {
         let rollupConfigData = fs.readFileSync(rollupConfigPath, 'utf8');
-        let updatedRollupConfigData = rollupConfigData.replace(new RegExp(sourceDirName, 'g'), targetDirName);
+        let updatedRollupConfigData = rollupConfigData.replace(new RegExp(sourceName, 'g'), targetName);
         fs.writeFileSync(rollupConfigPath, updatedRollupConfigData);
-        console.log('Updated rollup.config.js with new directory name.');
       } catch (error) {
         console.error('Error processing rollup.config.js:', error);
       }
     }
 
+    // Replace any instances of sourceDir with targetDir in HTML files
     if (file.includes('.html')) {
-      const htmlPath = path.join(target, `${targetDirName}.html`);
+      const htmlPath = path.join(target, `${targetName}.html`);
 
-      console.log('htmlPath', htmlPath);
       try {
         let htmlFile = fs.readFileSync(htmlPath, 'utf8');
-        let updatedHtmlFile = htmlFile.replace(new RegExp(sourceDirName, 'g'), targetDirName);
+        let updatedHtmlFile = htmlFile.replace(new RegExp(sourceName, 'g'), targetName);
         fs.writeFileSync(htmlPath, updatedHtmlFile);
-        console.log('Updated html with new directory name.');
       } catch (error) {
         console.error('Error processing rollup.config.js:', error);
       }
@@ -86,7 +85,7 @@ function copyDirectory(source, target) {
 }
 
 function createTargetPath(file, target) {
-  let targetFileName = file.replace(new RegExp(`^${sourceDirName}(.*)\\.(html|ts)$`), `${targetDirName}$1.$2`);
+  let targetFileName = file.replace(new RegExp(`^${sourceName}(.*)\\.(html|ts)$`), `${targetName}$1.$2`);
 
   return path.join(target, targetFileName);
 }
