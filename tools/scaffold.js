c3af88c68161d58f027d0c8b2576a7de4dc103a1
Daniel Imal <mrdanimal@gmail.com>
2024-05-13 14:09:44 -0400

    reoganization

--- tools/scaffold.js
+++ tools/scaffold.js
@@ -13,18 +13,13 @@ const sourceDir = path.join(__dirname, '../sketches', sourceName);
 const targetDir = path.join(__dirname, '../sketches', targetName);
 
 copyDirectory(sourceDir, targetDir);
+install();
 
 console.log(`Sketch './sketches/${targetName}' created.`);
 
 function copyDirectory(source, target) {
   if (!fs.existsSync(target)) {
     fs.mkdirSync(target);
-  }
-
-  try {
-    execSync(`cd ./sketches/${targetName} && yarn install`, { stdio: 'inherit' });
-  } catch (error) {
-    console.error('Error when running yarn install:', error);
   }
 
   const files = fs.readdirSync(source);
@@ -44,46 +39,66 @@ function copyDirectory(source, target) {
     }
 
     if (file === 'package.json') {
-      const packageJsonPath = path.join(target, file);
-
-      try {
-        let packageData = fs.readFileSync(packageJsonPath, 'utf8');
-        let packageJson = JSON.parse(packageData);
-        packageJson.name = targetName; // Replace the `name` field with `targetDirName`
-        fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2)); // Write back to the same file with formatting
-      } catch (error) {
-        console.error('Error processing package.json:', error);
-      }
+      setPackageName(target, file);
     }
 
     // Replace any instances of sourceDir with targetDir in rollup files
-    // TODO: find a less hacky way to handle this
     if (file === 'rollup.config.js') {
-      const rollupConfigPath = path.join(target, file);
-
-      try {
-        let rollupConfigData = fs.readFileSync(rollupConfigPath, 'utf8');
-        let updatedRollupConfigData = rollupConfigData.replace(new RegExp(sourceName, 'g'), targetName);
-        fs.writeFileSync(rollupConfigPath, updatedRollupConfigData);
-      } catch (error) {
-        console.error('Error processing rollup.config.js:', error);
-      }
+      fixRollupConfig(target, file);
     }
 
     // Replace any instances of sourceDir with targetDir in HTML files
-    // TODO: find a less hacky way to handle this
     if (file.includes('.html')) {
-      const htmlPath = path.join(target, `${targetName}.html`);
-
-      try {
-        let htmlFile = fs.readFileSync(htmlPath, 'utf8');
-        let updatedHtmlFile = htmlFile.replace(new RegExp(sourceName, 'g'), targetName);
-        fs.writeFileSync(htmlPath, updatedHtmlFile);
-      } catch (error) {
-        console.error('Error processing rollup.config.js:', error);
-      }
+      fixHtmlFile(target);
     }
   });
+}
+
+// TODO: find a less hacky way to handle this
+function fixHtmlFile(target) {
+  const htmlPath = path.join(target, `${targetName}.html`);
+
+  try {
+    let htmlFile = fs.readFileSync(htmlPath, 'utf8');
+    let updatedHtmlFile = htmlFile.replace(new RegExp(sourceName, 'g'), targetName);
+    fs.writeFileSync(htmlPath, updatedHtmlFile);
+  } catch (error) {
+    console.error('Error processing rollup.config.js:', error);
+  }
+}
+
+// TODO: find a less hacky way to handle this
+function fixRollupConfig(target, file) {
+  const rollupConfigPath = path.join(target, file);
+
+  try {
+    let rollupConfigData = fs.readFileSync(rollupConfigPath, 'utf8');
+    let updatedRollupConfigData = rollupConfigData.replace(new RegExp(sourceName, 'g'), targetName);
+    fs.writeFileSync(rollupConfigPath, updatedRollupConfigData);
+  } catch (error) {
+    console.error('Error processing rollup.config.js:', error);
+  }
+}
+
+function setPackageName(target, file) {
+  const packageJsonPath = path.join(target, file);
+
+  try {
+    let packageData = fs.readFileSync(packageJsonPath, 'utf8');
+    let packageJson = JSON.parse(packageData);
+    packageJson.name = targetName; // Replace the `name` field with `targetDirName`
+    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2)); // Write back to the same file with formatting
+  } catch (error) {
+    console.error('Error processing package.json:', error);
+  }
+}
+
+function install() {
+  try {
+    execSync(`cd ./sketches/${targetName} && yarn install`, { stdio: 'inherit' });
+  } catch (error) {
+    console.error('Error when running yarn install:', error);
+  }
 }
 
 function createTargetPath(file, target) {
