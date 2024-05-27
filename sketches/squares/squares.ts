defca925bb97d6b5504c42ac360a13aa71b4037d
Daniel Imal <mrdanimal@gmail.com>
2024-05-27 14:00:06 -0400

    remove color-disc stuff

--- sketches/squares/squares.ts
+++ sketches/squares/squares.ts
@@ -8,36 +8,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
   let color = localStorage.getItem('color');
 
-  const picker = document.getElementById('color-picker');
-
-  if (!color && picker) {
-    color = picker.getAttribute('color');
-    console.log('initial color', color);
-
-    picker.setAttribute('color', color);
-    console.log('set color', picker.getAttribute('color'));
-  }
-  if (color) {
-    picker.setAttribute('color', color);
-    console.log('set color', picker.getAttribute('color'));
-  }
-
-  if (picker) {
-    const observer = new MutationObserver((mutationsList) => {
-      for (const mutation of mutationsList) {
-        if (mutation.type === 'attributes' && mutation.attributeName === 'color') {
-          const newColor = picker.getAttribute('color');
-          localStorage.setItem('color', newColor);
-
-          render(context, newColor);
-        }
-      }
-      console.log(picker.getAttribute('color'));
-    });
-
-    observer.observe(picker, { attributes: true });
-  }
-
   render(context, color);
 });
 
@@ -45,7 +15,6 @@ function render(context: CanvasRenderingContext2D, baseColor: string) {
   const { width, height } = context.canvas;
   const center: IPointTuple = [width / 2, height / 2];
 
-  //  Do work here
   context.fillStyle = baseColor;
   context.save();
   context.translate(width / 4, height / 4);
