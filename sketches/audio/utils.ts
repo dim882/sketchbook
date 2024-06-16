f378b4b4e93dc27c59547249539c5e0f4d04129f
Daniel Imal <mrdanimal@gmail.com>
2024-06-16 07:07:40 -0400

    cleanup

--- sketches/audio/utils.ts
+++ sketches/audio/utils.ts
@@ -5,15 +5,15 @@ export function loop(context: CanvasRenderingContext2D, render: IRenderFunc, fps
   let frameDuration = 1000 / fps;
   let lastFrameTime = 0;
 
-  let t = 0;
+  let frame = 0;
   function animate(time: number) {
     requestAnimationFrame(animate);
 
     if (time - lastFrameTime < frameDuration) return;
     lastFrameTime = time;
 
-    render(context, t);
-    t++;
+    render(context, frame);
+    frame++;
   }
 
   requestAnimationFrame(animate);
