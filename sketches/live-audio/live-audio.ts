5581a1f36a396ed453d1d5dcf5a0842d06bf1c33
Daniel Imal <mrdanimal@gmail.com>
2024-06-17 16:40:40 -0400

    delete old cruft

--- sketches/live-audio/live-audio.ts
+++ sketches/live-audio/live-audio.ts
@@ -5,7 +5,7 @@ import {
   renderWaveform,
   setupAudioContext,
 } from './live-audio.utils.js';
-import { IPointTuple, loop } from './utils.js';
+import { loop } from './utils.js';
 
 window.addEventListener('DOMContentLoaded', async () => {
   const canvas = document.getElementById('canvas') as HTMLCanvasElement;
@@ -37,41 +37,4 @@ async function startRenderingWaveformForDevice(
 
   // Integrate with the existing animation loop
   loop(canvasContext, renderFunction, 60); // Assuming 60 FPS
-}
-function render(context: CanvasRenderingContext2D, t: number) {
-  const { width, height } = context.canvas;
-  const center: IPointTuple = [width / 2, height / 2];
-
-  const radius = Math.floor(Math.abs(Math.sin(t * 0.05) * 100));
-
-  context.clearRect(0, 0, width, height);
-
-  context.fillStyle = '#fff';
-  context.fillRect(0, 0, width, height);
-
-  context.beginPath();
-  context.arc(...center, radius, 0, 2 * Math.PI);
-  context.fillStyle = 'red';
-  context.fill();
-}
-
-const playAudio = async (audioContext: AudioContext, source: AudioBufferSourceNode, audioElement: HTMLAudioElement) => {
-  try {
-    const audioBuffer = await createAudioBuffer(audioElement, audioContext);
-
-    source.buffer = audioBuffer;
-    source.connect(audioContext.destination);
-    source.start();
-
-    console.log('Audio started playing');
-  } catch (error) {
-    console.error('Error playing audio:', error);
-  }
-  return source;
-};
-
-async function createAudioBuffer(audioElement: HTMLAudioElement, audioContext: AudioContext) {
-  const track = await fetch(audioElement.src);
-  const arrayBuffer = await track.arrayBuffer();
-  return await audioContext.decodeAudioData(arrayBuffer);
 }
