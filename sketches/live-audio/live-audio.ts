588452d8ef856e751f0c67f671078d6a08fb8ebc
Daniel Imal <mrdanimal@gmail.com>
2024-06-18 16:26:17 -0400

    cleanupp

--- sketches/live-audio/live-audio.ts
+++ sketches/live-audio/live-audio.ts
@@ -18,7 +18,6 @@ window.addEventListener('DOMContentLoaded', async () => {
   audioDevices.forEach((device) => {});
   const deviceId = 'cfb9b5a09ecdbe210d3277457cb76ffcc83dca38555bd88b97982a333266df20';
   const stream = await captureAudioStream(deviceId);
-
   const render = startRenderingWaveformForDevice(stream, '#ff0000');
 
   loop(context, render, 60);
@@ -27,9 +26,9 @@ window.addEventListener('DOMContentLoaded', async () => {
 function startRenderingWaveformForDevice(stream: MediaStream, color: string) {
   const { audioContext, sourceNode } = setupAudioContext(stream);
   const analyser = createAnalyser(audioContext);
-  sourceNode.connect(analyser);
-
   const dataArray = new Uint8Array(analyser.frequencyBinCount);
+
+  sourceNode.connect(analyser);
 
   return (context: CanvasRenderingContext2D, _t: number) => {
     analyser.getByteTimeDomainData(dataArray);
