a3f41ed353de183d715781cc88aca4a295427e40
Daniel Imal <mrdanimal@gmail.com>
2024-06-18 16:24:56 -0400

    return the render func

--- sketches/live-audio/live-audio.ts
+++ sketches/live-audio/live-audio.ts
@@ -19,23 +19,21 @@ window.addEventListener('DOMContentLoaded', async () => {
   const deviceId = 'cfb9b5a09ecdbe210d3277457cb76ffcc83dca38555bd88b97982a333266df20';
   const stream = await captureAudioStream(deviceId);
 
-  startRenderingWaveformForDevice(context, stream, '#00ff00');
+  const render = startRenderingWaveformForDevice(stream, '#ff0000');
+
+  loop(context, render, 60);
 });
 
-async function startRenderingWaveformForDevice(
-  canvasContext: CanvasRenderingContext2D,
-  stream: MediaStream,
-  color: string
-): Promise<void> {
+function startRenderingWaveformForDevice(stream: MediaStream, color: string) {
   const { audioContext, sourceNode } = setupAudioContext(stream);
   const analyser = createAnalyser(audioContext);
   sourceNode.connect(analyser);
 
-  const renderFunction = (context: CanvasRenderingContext2D, _t: number) => {
+  const render = (context: CanvasRenderingContext2D, _t: number) => {
     const dataArray = new Uint8Array(analyser.frequencyBinCount);
     analyser.getByteTimeDomainData(dataArray);
+
     renderWaveform(context, dataArray, color);
   };
-
-  loop(canvasContext, renderFunction, 60); // Assuming 60 FPS
+  return render;
 }
