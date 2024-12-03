a88b63fd3bc2c8dfff268d0c6449d27b7ac48032
Daniel Imal <mrdanimal@gmail.com>
2024-12-02 19:12:15 -0500

    draw a lot of squares

--- sketches/square-transform/square-transform.ts
+++ sketches/square-transform/square-transform.ts
@@ -2,6 +2,7 @@ export type PseudoRandomNumberGenerator = () => number;
 export type IPointTuple = [number, number];
 
 const SQUARE_SIZE = 200;
+const SQUARE_COUNT = 10;
 
 window.addEventListener('DOMContentLoaded', () => {
   const canvas = document.getElementById('canvas') as HTMLCanvasElement;
@@ -19,12 +20,16 @@ function render(context: CanvasRenderingContext2D) {
   context.fillStyle = '#fff';
   context.fillRect(0, 0, width, height);
 
-  drawSquareOutline(context, 20, centerY - SQUARE_SIZE / 2, SQUARE_SIZE);
+  const stepX = (width - 20 - SQUARE_SIZE) / SQUARE_COUNT;
+
+  Array.from({ length: SQUARE_COUNT }).forEach((_, i) => {
+    drawSquare(context, 20 + stepX * i, centerY - SQUARE_SIZE / 2, SQUARE_SIZE);
+  });
 
   context.restore();
 }
 
-function drawSquareOutline(context: CanvasRenderingContext2D, x: number, y: number, size: number) {
+function drawSquare(context: CanvasRenderingContext2D, x: number, y: number, size: number) {
   context.strokeStyle = '#000';
   context.strokeRect(x, y, size, size);
 }
