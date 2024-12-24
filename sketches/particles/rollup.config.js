1dc0116c1e59f6e58b0851169c1234cade986d52
Daniel Imal <mrdanimal@gmail.com>
2024-12-24 12:49:55 -0500

    fix type errors

---
+++ sketches/particles/rollup.config.js
@@ -0,0 +1,12 @@
+import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+export default {
+  input: 'particles.ts',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'module',
+    sourcemap: true,
+  },
+  plugins: [nodeResolve(), typescript()],
+};
