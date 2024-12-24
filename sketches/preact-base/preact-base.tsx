7152c0866d3c061f3645b84d10ad694ec6d092d8
Daniel Imal <mrdanimal@gmail.com>
2024-12-24 12:46:34 -0500

    upgrade preact

--- sketches/preact-base/preact-base.tsx
+++ sketches/preact-base/preact-base.tsx
@@ -1,5 +1,6 @@
 import { render } from 'preact';
 import App from './App';
+import { useTransition } from 'preact/compat';
 
 window.addEventListener('DOMContentLoaded', () => {
   render(<App />, document.getElementById('app') as Element);
