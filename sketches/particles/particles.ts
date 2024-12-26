4bdb5c82599f7b170f901d941496d88dcb1fe99f
Daniel Imal <mrdanimal@gmail.com>
2024-12-26 18:53:46 -0500

    cleanup

--- sketches/particles/particles.ts
+++ sketches/particles/particles.ts
@@ -24,16 +24,4 @@ document.body.onload = () => {
 function render(context: CanvasRenderingContext2D, t: number) {
   const { width, height } = context.canvas;
   const center: IPointTuple = [width / 2, height / 2];
-
-  const radius = Math.floor(Math.abs(Math.sin(t * 0.05) * 100));
-
-  context.clearRect(0, 0, width, height);
-
-  context.fillStyle = '#fff';
-  context.fillRect(0, 0, width, height);
-
-  context.beginPath();
-  context.arc(...center, radius, 0, 2 * Math.PI);
-  context.fillStyle = 'red';
-  context.fill();
 }
