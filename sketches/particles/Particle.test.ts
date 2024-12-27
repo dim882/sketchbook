f70087fb2e38ecd8efef28b96263d039cb48037d
Daniel Imal <mrdanimal@gmail.com>
2024-12-27 14:21:54 -0500

    fill in tests for particle. fail

--- sketches/particles/Particle.test.ts
+++ sketches/particles/Particle.test.ts
@@ -2,29 +2,44 @@ import * as Particle from './Particle';
 import * as Vector from './Vector';
 
 describe('Particle', () => {
-  describe('create', () => {
-    it('should create a particle with default values', () => {
-      const position = Vector.create(0, 0);
-      const particle = Particle.create({ position });
+  describe('applyForce', () => {
+    it('should apply force to particle acceleration', () => {
+      const particle = Particle.create({ position: Vector.create(0, 0) });
+      const force = Vector.create(1, 1);
 
-      expect(particle.position).toEqual(position);
-      expect(particle.velocity).toEqual(Vector.create(0, 0));
-      expect(particle.acceleration).toEqual(Vector.create(0, 0));
-      expect(particle.radius).toBe(1);
+      const updatedParticle = Particle.applyForce(particle, force);
+
+      expect(updatedParticle.acceleration).toEqual(Vector.create(1, 1));
     });
   });
 
   describe('update', () => {
     it('should update particle position and velocity', () => {
-      // Test implementation
+      const particle = Particle.create({
+        position: Vector.create(0, 0),
+        velocity: Vector.create(1, 1),
+        acceleration: Vector.create(0.5, 0.5),
+      });
+
+      Particle.update(particle);
+
+      expect(particle.velocity).toEqual(Vector.create(1.5, 1.5));
+      expect(particle.position).toEqual(Vector.create(1.5, 1.5));
+      expect(particle.acceleration).toEqual(Vector.create(0, 0));
+    });
+
+    it('should limit velocity when maxVelocity is provided', () => {
+      const particle = Particle.create({
+        position: Vector.create(0, 0),
+        velocity: Vector.create(2, 2),
+        acceleration: Vector.create(1, 1),
+      });
+
+      Particle.update(particle, 2);
+
+      const velocityMagnitude = Vector.getMagnitude(particle.velocity);
+      expect(velocityMagnitude).toBeLessThanOrEqual(2);
+      expect(Vector.equals(particle.velocity, Vector.setMagnitude(particle.velocity, 2))).toBe(true);
     });
   });
-
-  describe('applyForce', () => {
-    it('should apply force to particle acceleration', () => {
-      // Test implementation
-    });
-  });
-
-  // Add more test cases for other functions
 });
