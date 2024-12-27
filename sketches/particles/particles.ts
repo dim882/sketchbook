44e004e481531506b4137a174de3437ed6cdcbec
Daniel Imal <mrdanimal@gmail.com>
2024-12-26 20:42:52 -0500

    more tidying

--- sketches/particles/particles.ts
+++ sketches/particles/particles.ts
@@ -2,9 +2,6 @@ import { type IParticle, create } from './Particle.js';
 import { getCanvas, getCanvasContext, type IPointTuple, loop } from './particles.utils.js';
 import { fromAngle, fromTuple, type IVector, toTuple } from './Vector.js';
 
-let particle: IParticle;
-let force: IVector;
-
 document.body.onload = () => {
   const canvas = getCanvas();
   const context = getCanvasContext(canvas);
@@ -12,8 +9,8 @@ document.body.onload = () => {
   const { width, height } = context.canvas;
   const center: IPointTuple = [width / 2, height / 2];
 
-  particle = create({ position: fromTuple(center) });
-  force = fromAngle(1);
+  const particle = create({ position: fromTuple(center) });
+  const force = fromAngle(1);
 
   loop(context, createRender(context, { particle, force }), 60);
 };
@@ -23,11 +20,13 @@ interface ISetupData {
   force: IVector;
 }
 
-const createRender = (context: CanvasRenderingContext2D, setupData: ISetupData) => (t: number) => {
-  console.log({ setupData });
+const createRender =
+  (context: CanvasRenderingContext2D, { particle, force }: ISetupData) =>
+  (t: number) => {
+    console.log({ particle, force });
 
-  context.beginPath();
-  context.arc(...toTuple(particle.position), 10, 0, 2 * Math.PI);
-  context.fillStyle = 'red';
-  context.fill();
-};
+    context.beginPath();
+    context.arc(...toTuple(particle.position), 10, 0, 2 * Math.PI);
+    context.fillStyle = 'red';
+    context.fill();
+  };
