842dd41b8ef8274764e1e2c0411366c91c0491bd
Daniel Imal <mrdanimal@gmail.com>
2024-12-26 20:38:39 -0500

    tighten up vector import

--- sketches/particles/Particle.ts
+++ sketches/particles/Particle.ts
@@ -1,37 +1,30 @@
-import {
-  type IVector,
-  clone as cloneVector,
-  multiply as multiplyVector,
-  limit as limitVector,
-  add as addVector,
-  create,
-} from './Vector';
+import * as Vector from './Vector';
 
 export interface IParticle {
-  previousPosition: IVector;
-  position: IVector;
-  velocity: IVector;
-  acceleration: IVector;
+  previousPosition: Vector.IVector;
+  position: Vector.IVector;
+  velocity: Vector.IVector;
+  acceleration: Vector.IVector;
   maxVelocity?: number;
   radius: number;
 }
 
 interface IParticleConstructor {
-  position: IVector;
-  velocity?: IVector;
-  acceleration?: IVector;
+  position: Vector.IVector;
+  velocity?: Vector.IVector;
+  acceleration?: Vector.IVector;
   maxVelocity?: number;
   radius?: number;
 }
 
 export const createParticle = ({
   position,
-  velocity = create(0, 0),
-  acceleration = create(0, 0),
+  velocity = Vector.create(0, 0),
+  acceleration = Vector.create(0, 0),
   maxVelocity,
   radius = 1,
 }: IParticleConstructor): IParticle => ({
-  previousPosition: cloneVector(position),
+  previousPosition: Vector.clone(position),
   position,
   velocity,
   acceleration,
@@ -40,27 +33,27 @@ export const createParticle = ({
 });
 
 export const updateParticle = (particle: IParticle, maxVelocity?: number): void => {
-  addVector(particle.velocity, particle.acceleration);
+  Vector.add(particle.velocity, particle.acceleration);
 
   const mVelocity = maxVelocity ?? particle.maxVelocity;
 
   if (mVelocity) {
-    limitVector(particle.velocity, mVelocity);
+    Vector.limit(particle.velocity, mVelocity);
   }
 
-  addVector(particle.position, particle.velocity);
-  multiplyVector(particle.acceleration, 0);
+  Vector.add(particle.position, particle.velocity);
+  Vector.multiply(particle.acceleration, 0);
 };
 
-export const applyForceToParticle = (particle: IParticle, force: IVector): void => {
-  addVector(particle.acceleration, force);
+export const applyForceToParticle = (particle: IParticle, force: Vector.IVector): void => {
+  Vector.add(particle.acceleration, force);
 };
 
 export const updateParticlePreviousPosition = (particle: IParticle): void => {
-  particle.previousPosition = cloneVector(particle.position);
+  particle.previousPosition = Vector.clone(particle.position);
 };
 
 export const copyParticle = (particle: IParticle): IParticle =>
-  createParticle({ position: cloneVector(particle.position) });
+  createParticle({ position: Vector.clone(particle.position) });
 
 export default createParticle;
