8b134fa3c4b9e827e7d9053ce182f1ccb77c2428
Daniel Imal <mrdanimal@gmail.com>
2025-01-12 18:18:59 -0500

    draw some lines

--- sketches/axis/axis.ts
+++ sketches/axis/axis.ts
@@ -3,6 +3,14 @@ import { range, traceArc } from './axis.utils';
 export type PseudoRandomNumberGenerator = () => number;
 export type IPointTuple = [number, number];
 export type IQuadrant = [number, number];
+export type IQuadrants = [IQuadrant, IQuadrant, IQuadrant, IQuadrant];
+
+const QUADRANTS: IQuadrants = [
+  [0, Math.PI / 2],
+  [Math.PI / 2, Math.PI],
+  [Math.PI, (Math.PI / 2) * 3],
+  [(Math.PI / 2) * 3, 0],
+];
 
 const getFloat = (generateNumber: PseudoRandomNumberGenerator, lower = 0, upper = 1) => {
   return (upper - lower) * generateNumber() + lower;
@@ -23,15 +31,6 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 });
 
-export type IQuadrants = [IQuadrant, IQuadrant, IQuadrant, IQuadrant];
-
-const QUADRANTS: IQuadrants = [
-  [0, Math.PI / 2],
-  [Math.PI / 2, Math.PI],
-  [Math.PI, (Math.PI / 2) * 3],
-  [(Math.PI / 2) * 3, 0],
-];
-
 function render(context: CanvasRenderingContext2D) {
   const { width, height } = context.canvas;
   const center: IPointTuple = [width / 2, height / 2];
@@ -40,13 +39,21 @@ function render(context: CanvasRenderingContext2D) {
   context.translate(...center);
 
   const radius = Math.min(width, height) * 0.2;
-  context.lineWidth = 1;
 
   traceArc(context, radius, ...QUADRANTS[0], 20);
   traceArc(context, radius, ...QUADRANTS[2], 100);
 
   range(100, radius, 10).forEach((i) => traceArc(context, i, ...QUADRANTS[1], 1));
   range(150, radius, 20).forEach((i) => traceArc(context, i, ...QUADRANTS[3], 1));
+
+  range(0, Math.PI * 2, Math.PI / 2).forEach((angle) => {
+    context.save();
+    context.rotate(angle);
+    context.moveTo(0, 0);
+    context.lineTo((width / 2) * 0.6, 0);
+    context.stroke();
+    context.restore();
+  });
 
   context.restore();
 }
