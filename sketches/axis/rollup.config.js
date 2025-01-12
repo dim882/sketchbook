7bed894ed43b463790e5026f43e61d199cdfa016
Daniel Imal <mrdanimal@gmail.com>
2025-01-12 15:49:25 -0500

    stub new sketch

---
+++ sketches/axis/rollup.config.js
@@ -0,0 +1,24 @@
+import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import copy from 'rollup-plugin-copy';
+
+export default {
+  input: 'axis.ts',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'es',
+    sourcemap: true,
+  },
+  plugins: [
+    nodeResolve(),
+    typescript({
+      include: ['../../lib/**/*.ts', '**/*.ts'],
+    }),
+    copy({
+      targets: [
+        { src: '*.css', dest: 'dist' },
+        { src: '*.html', dest: 'dist' },
+      ],
+    }),
+  ],
+};
