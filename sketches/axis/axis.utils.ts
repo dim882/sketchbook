705fab8c64d8eb6d7707b343c2ff5c7e775d6023
Daniel Imal <mrdanimal@gmail.com>
2025-01-12 19:00:03 -0500

    delete comment

--- sketches/axis/axis.utils.ts
+++ sketches/axis/axis.utils.ts
@@ -14,8 +14,3 @@ export function traceArc(
 
 export const range = (start: number, end: number, step: number = 1): number[] =>
   Array.from({ length: Math.ceil((end - start) / step) }, (_, i) => start + i * step);
-
-// Usage examples:
-// console.log(range(0, 5));        // [0, 1, 2, 3, 4]
-// console.log(range(1, 10, 2));    // [1, 3, 5, 7, 9]
-// console.log(range(5, 0, -1));    // [5, 4, 3, 2, 1]
