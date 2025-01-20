04692476d9abdfddbc87817073ab01eb6e1226d7
Daniel Imal <mrdanimal@gmail.com>
2025-01-20 15:59:30 -0500

    move logic to preact component

--- sketches/blob-grid-svg/BlobGrid.tsx
+++ sketches/blob-grid-svg/BlobGrid.tsx
@@ -1,8 +1,13 @@
 import { JSX } from 'preact/jsx-runtime';
 import { useState, useEffect } from 'preact/hooks';
+import { createGrid, getInteger } from './blob-grid-svg.utils';
 
+type IDimensions = {
+  width: number;
+  height: number;
+};
 function App(): JSX.Element {
-  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+  const [dimensions, setDimensions] = useState<IDimensions>({ width: 0, height: 0 });
 
   useEffect(() => {
     const updateDimensions = () => {
@@ -21,6 +26,17 @@ function App(): JSX.Element {
   }, []);
 
   console.log(dimensions);
+
+  const formHue = getInteger(prng, 0, 270);
+  const backgroundHue = formHue + 180;
+
+  const backgroundColor = `lch(95% 40% ${backgroundHue})`;
+  const fillColor = `lch(40% 50% ${formHue})`;
+
+  const CELL_SIZE = 250;
+  const RADIUS = 100;
+
+  const grid = createGrid(dimensions.width, dimensions.height, CELL_SIZE);
 
   return (
     <div class="blobs">
--- sketches/blob-grid-svg/blob-grid-svg.tsx
+++ sketches/blob-grid-svg/blob-grid-svg.tsx
@@ -1,22 +1,6 @@
-import { getInteger, createGrid } from './blob-grid-svg.utils.js';
 import { render } from 'preact';
-import App from './App';
-
-function run() {
-  const formHue = getInteger(prng, 0, 270);
-  const backgroundHue = formHue + 180;
-
-  const backgroundColor = `lch(95% 40% ${backgroundHue})`;
-  const fillColor = `lch(40% 50% ${formHue})`;
-
-  const CELL_SIZE = 250;
-  const RADIUS = 100;
-
-  const grid = createGrid(width, height, CELL_SIZE);
-}
+import BlobGrid from './BlobGrid.js';
 
 window.addEventListener('DOMContentLoaded', () => {
-  console.log('DOMContentLoaded?!');
-
-  render(<App />, document.getElementById('app') as Element);
+  render(<BlobGrid />, document.getElementById('app') as Element);
 });
