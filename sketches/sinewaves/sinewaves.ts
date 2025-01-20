4044d8bda0bcd68b9f97a9c4618f62ef43e9f655
Daniel Imal <mrdanimal@gmail.com>
2025-01-20 12:23:14 -0500

    render a sine wave

--- sketches/sinewaves/sinewaves.ts
+++ sketches/sinewaves/sinewaves.ts
@@ -4,22 +4,26 @@ document.body.onload = () => {
   const canvas = document.getElementById('canvas') as HTMLCanvasElement;
   const context = canvas.getContext('2d');
 
-  loop(context, render, 60);
+  if (context) {
+    loop(context, render, 60);
+  }
 };
 
 function render(context: CanvasRenderingContext2D, t: number) {
   const { width, height } = context.canvas;
   const center: IPointTuple = [width / 2, height / 2];
 
-  const radius = Math.floor(Math.abs(Math.sin(t * 0.05) * 100));
-
-  context.clearRect(0, 0, width, height);
-
   context.fillStyle = '#fff';
   context.fillRect(0, 0, width, height);
 
   context.beginPath();
-  context.arc(...center, radius, 0, 2 * Math.PI);
-  context.fillStyle = 'red';
-  context.fill();
+  context.strokeStyle = '#000';
+  context.lineWidth = 2;
+
+  for (let y = 0; y < height; y++) {
+    const x = center[0] + Math.sin(y * 0.05 + t * 0.1) * 50;
+    context.lineTo(x, y);
+  }
+
+  context.stroke();
 }
