b7313a2f914166465410a71ea2015427824c6314
Daniel Imal <mrdanimal@gmail.com>
2025-01-23 17:32:25 -0500

    start to make way for SSR

--- tools/server.ts
+++ tools/server.ts
@@ -32,9 +32,6 @@ app.get('/', (req, res) => {
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
       <link href="https://fonts.googleapis.com/css2?family=Anybody:wght@100&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
 
-      <style>
-      </style>
-
       <div class="container">
         <div class="list">
           <h1>Sketches</h1>
