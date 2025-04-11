cdb16fd78261999e1bdff90bb56c96418bf9ea03
Daniel Imal <mrdanimal@gmail.com>
2025-04-11 17:37:32 -0400

    render the grid correctly

--- sketches/palettes/color-grid.tsx
+++ sketches/palettes/color-grid.tsx
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import { createComponent, EventHandler, jsx, RenderFunc } from 'functron';
+import { createComponent, jsx, RenderFunc } from 'functron';
 import { getPalette, IPalette } from './palettes/personal-site';
 import './color-grid.css';
 
@@ -13,32 +13,30 @@ const initialModel: ICounterModel = {
 
 const handlers = {};
 
-const render: RenderFunc<ICounterModel> = ({ palette }, {}) => (
-  <div>
+const render: RenderFunc<ICounterModel> = ({ palette }, {}) => {
+  const colorEntries = Object.entries(palette);
+
+  return (
     <div class={{ 'color-grid-container': true }}>
-      {Object.entries(palette).map(([colorName, colorValues]) => (
-        <div key={colorName} class={{ 'color-category': true }}>
-          <h3 class={{ 'color-category-title': true }}>{colorName}</h3>
-          <div class={{ 'color-swatches': true }}>
-            {colorValues.map((color, index) => (
-              <div
-                key={`${colorName}-${index}`}
-                class={{
-                  'color-swatch': true,
-                  'light-text': index < 4,
-                  'dark-text': index >= 4,
-                }}
-                style={{ backgroundColor: color }}
-              >
-                {color}
-              </div>
-            ))}
+      {colorEntries.flatMap(([colorName, colorValues]) => [
+        <div class={{ 'color-name': true }}>{colorName}</div>,
+        ...colorValues.map((color, index) => (
+          <div
+            key={`${colorName}-${index}`}
+            class={{
+              'color-swatch': true,
+              'light-text': index < 4,
+              'dark-text': index >= 4,
+            }}
+            style={{ backgroundColor: color }}
+          >
+            {color}
           </div>
-        </div>
-      ))}
+        )),
+      ])}
     </div>
-  </div>
-);
+  );
+};
 
 const ColorGrid = createComponent<[], ICounterModel, typeof handlers, typeof render>({
   initialModel,
