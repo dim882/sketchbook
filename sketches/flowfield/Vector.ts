3bb310fedbc273a81a91262f7355a52713703c5c
Daniel Imal <mrdanimal@gmail.com>
2025-04-29 21:29:50 -0400

    use particles as the base instead

---
+++ sketches/flowfield/Vector.ts
@@ -0,0 +1,115 @@
+import type { IPointTuple } from './particles.utils';
+
+export interface IVector {
+  x: number;
+  y: number;
+  z?: number;
+}
+
+export const create = (x: number, y: number, z?: number): IVector => (z === undefined ? { x, y } : { x, y, z });
+
+export const fromRadians = (angle: number) => create(Math.cos(angle), Math.sin(angle));
+
+export const add = (v1: IVector, v2: IVector): IVector => {
+  const z = v1.z !== undefined && v2.z !== undefined ? v1.z + v2.z : v1.z;
+
+  const vector = {
+    x: v1.x + v2.x,
+    y: v1.y + v2.y,
+  };
+
+  return z ? { ...vector, z } : vector;
+};
+
+export const subtract = (v1: IVector, v2: IVector): IVector => {
+  const x = v1.x - v2.x;
+  const y = v1.y - v2.y;
+  const z = v1.z !== undefined && v2.z !== undefined ? v1.z - v2.z : v1.z;
+
+  return { x, y, z };
+};
+
+export const multiply = (v: IVector | number, factor: IVector | number): IVector => {
+  if (typeof v === 'number') {
+    const x = v * (typeof factor === 'number' ? factor : factor.x);
+    const y = v * (typeof factor === 'number' ? factor : factor.y);
+    const z = v * (typeof factor === 'number' ? factor : factor.z || 1);
+
+    return { x, y, z };
+  }
+
+  const x = v.x * (typeof factor === 'number' ? factor : factor.x);
+  const y = v.y * (typeof factor === 'number' ? factor : factor.y);
+  const z = v.z ? (typeof factor === 'number' ? factor : factor.z || 1) : v.z;
+
+  return { x, y, z };
+};
+
+export const divide = (v: IVector | number, divisor: IVector | number): IVector => {
+  if (typeof v === 'number') {
+    const x = v / (typeof divisor === 'number' ? divisor : divisor.x);
+    const y = v / (typeof divisor === 'number' ? divisor : divisor.y);
+    const z = v / (typeof divisor === 'number' ? divisor : divisor.z || 1);
+
+    return { x, y, z };
+  }
+  const x = v.x / (typeof divisor === 'number' ? divisor : divisor.x);
+  const y = v.y / (typeof divisor === 'number' ? divisor : divisor.y);
+  const z = v.z ? (typeof divisor === 'number' ? divisor : divisor.z || 1) : v.z;
+
+  return { x, y, z };
+};
+
+export const equals = (v1: IVector, v2: IVector): boolean => v1.x === v2.x && v1.y === v2.y && v1.z === v2.z;
+
+export const normalize = (v: IVector): IVector => {
+  const length = getMagnitude(v);
+
+  if (length !== 0) {
+    return multiply(v, 1 / length);
+  }
+
+  return create(0, 0, 0);
+};
+
+export const getMagnitudeSquared = (v: IVector): number => v.x * v.x + v.y * v.y + (v.z ? v.z * v.z : 0);
+
+export const setMagnitude = (v: IVector, magnitude: number): IVector => multiply(normalize(v), magnitude);
+
+export const getMagnitude = (v: IVector): number => Math.sqrt(getMagnitudeSquared(v));
+
+export const limit = (v: IVector, value: number): IVector => {
+  const magnitudeSquared = getMagnitudeSquared(v);
+
+  if (magnitudeSquared > value * value) {
+    return multiply(normalize(v), value);
+  }
+
+  return v;
+};
+
+export const length = (v: IVector): number => Math.sqrt(dot(v, v));
+
+export const dot = (v1: IVector, v2: IVector): number =>
+  v1.x * v2.x + v1.y * v2.y + (v1.z ? (v2.z !== undefined ? v1.z * v2.z : 0) : 0);
+
+// Is this correct?
+export const toArray = (v: IVector): number[] => [v.x, v.y, v.z ?? 0];
+
+export const clone = (v: IVector): IVector => create(v.x, v.y, v.z);
+
+export const toAngle = (v: IVector): number => Math.atan2(v.y, v.x);
+
+export const copy = (v: IVector): IVector => clone(v);
+
+export const fromAngle = (angleRadians: number, length = 1): IVector => {
+  const x = length * Math.cos(angleRadians);
+  const y = length * Math.sin(angleRadians);
+  const z = 0;
+
+  return { x, y, z };
+};
+
+export const fromTuple = ([x, y]: IPointTuple): IVector => create(x, y);
+
+export const toTuple = (v: IVector): IPointTuple => [v.x, v.y];
--- sketches/flowfield/flowfield.ts
+++ sketches/flowfield/flowfield.ts
@@ -1,27 +1,50 @@
-import { IPointTuple, loop } from './utils.js';
+import { type IParticle, create, applyForce } from './Particle.js';
+import { getCanvas, getCanvasContext, type IPointTuple, loop } from './flowfield.utils.js';
+import * as Vec from './Vector.js';
 
 document.body.onload = () => {
-  const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-  const context = canvas.getContext('2d');
+  const canvas = getCanvas();
+  const context = getCanvasContext(canvas);
+  const { width, height } = canvas;
+  const center: IPointTuple = [width / 2, height / 2];
+  const particle = create({ position: Vec.fromTuple(center) });
+  const previousTime = 0;
+  const force: Vec.IVector = { x: 0, y: 0 };
 
-  if (context) {
-    loop(context, render, 60);
-  }
+  loop(render(context, { particle, previousTime, force }), 60);
 };
 
-function render(context: CanvasRenderingContext2D, t: number) {
-  const { width, height } = context.canvas;
-  const center: IPointTuple = [width / 2, height / 2];
+interface ISketchData {
+  particle: IParticle;
+  previousTime: number;
+  force: Vec.IVector;
+}
 
-  const radius = Math.floor(Math.abs(Math.sin(t * 0.05) * 100));
+console.log('foo');
+
+const render = (context: CanvasRenderingContext2D, data: ISketchData) => (t: number) => {
+  const { width, height } = context.canvas;
+
+  if (t - data.previousTime >= 120) {
+    data.particle.velocity = { x: 0, y: 0 };
+
+    const angle = Math.random() * Math.PI * 2;
+    const newForce = 100;
+
+    data.force = Vec.multiply(Vec.fromAngle(angle), newForce);
+    data.previousTime = t;
+  }
+
+  data.particle = applyForce({
+    particle: data.particle,
+    force: data.force,
+    deltaTime: 1 / 60,
+  });
 
   context.clearRect(0, 0, width, height);
 
-  context.fillStyle = '#fff';
-  context.fillRect(0, 0, width, height);
-
   context.beginPath();
-  context.arc(...center, radius, 0, 2 * Math.PI);
-  context.fillStyle = 'red';
+  context.arc(...Vec.toTuple(data.particle.position), 10, 0, 2 * Math.PI);
+  context.fillStyle = 'purple';
   context.fill();
-}
+};
