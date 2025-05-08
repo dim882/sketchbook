81ccf45250715c87c9d9a161440994dc22407449
Daniel Imal <mrdanimal@gmail.com>
2025-05-08 16:31:35 -0400

    stub oop particles sketch

---
+++ sketches/particles-oop/particles-oop.ts
@@ -0,0 +1,49 @@
+import { type IParticle, create, applyForce } from './Particle.js';
+import { getCanvas, getCanvasContext, type IPointTuple, loop } from './particles-oop.utils.js';
+import * as Vec from './Vector.js';
+console.log('foo');
+
+document.body.onload = () => {
+  const canvas = getCanvas();
+  const context = getCanvasContext(canvas);
+  const { width, height } = canvas;
+  const center: IPointTuple = [width / 2, height / 2];
+  const particle = create({ position: Vec.fromTuple(center) });
+  const previousTime = 0;
+  const force: Vec.IVector = { x: 0, y: 0 };
+
+  loop(render(context, { particle, previousTime, force }), 60);
+};
+
+interface ISketchData {
+  particle: IParticle;
+  previousTime: number;
+  force: Vec.IVector;
+}
+
+const render = (context: CanvasRenderingContext2D, data: ISketchData) => (t: number) => {
+  const { width, height } = context.canvas;
+
+  if (t - data.previousTime >= 120) {
+    data.particle.velocity = { x: 0, y: 0 };
+
+    const angle = Math.random() * Math.PI * 2;
+    const newForce = 100;
+
+    data.force = Vec.multiply(Vec.fromAngle(angle), newForce);
+    data.previousTime = t;
+  }
+
+  data.particle = applyForce({
+    particle: data.particle,
+    force: data.force,
+    deltaTime: 1 / 60,
+  });
+
+  context.clearRect(0, 0, width, height);
+
+  context.beginPath();
+  context.arc(...Vec.toTuple(data.particle.position), 10, 0, 2 * Math.PI);
+  context.fillStyle = 'purple';
+  context.fill();
+};
---
+++ sketches/particles-oop/particles-oop.utils.ts
@@ -0,0 +1,34 @@
+export type IRenderFunc = (t: number) => void;
+export type IPointTuple = [number, number];
+
+export function loop(render: IRenderFunc, fps = 60) {
+  const frameDuration = 1000 / fps;
+  let lastFrameTime = 0;
+  let t = 0;
+
+  function animate(time: number) {
+    requestAnimationFrame(animate);
+
+    if (time - lastFrameTime < frameDuration) return;
+    lastFrameTime = time;
+
+    render(t); // Assuming `context` is accessible in this scope
+    t++;
+  }
+
+  requestAnimationFrame(animate);
+}
+
+export const getCanvas = (): HTMLCanvasElement => {
+  const canvas = document.querySelector('canvas');
+  if (!canvas) throw new Error('Canvas element not found');
+
+  return canvas;
+};
+
+export const getCanvasContext = (canvas: HTMLCanvasElement): CanvasRenderingContext2D => {
+  const context = canvas.getContext('2d');
+  if (!context) throw new Error('Could not get 2D context');
+
+  return context;
+};
