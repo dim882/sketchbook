f1aaf50e1a35a31bd637fd4ffd9980071036d263
Daniel Imal <mrdanimal@gmail.com>
2025-05-09 12:55:52 -0400

    tighten up

--- sketches/particles-oop/particles-oop.ts
+++ sketches/particles-oop/particles-oop.ts
@@ -1,6 +1,6 @@
 import { Particle } from './Particle';
 import { Vector } from './Vector';
-import { getCanvas, getCanvasContext, type IPointTuple, loop, handleEdges } from './particles-oop.utils.js';
+import { getCanvas, getCanvasContext, loop, handleEdges } from './particles-oop.utils.js';
 
 export const INITIAL_SPEED = 250;
 export const FPS = 60;
@@ -8,20 +8,18 @@ export const DELTA_TIME = 1 / FPS;
 
 interface ISketchData {
   particle: Particle;
-  width: number;
-  height: number;
 }
 
 document.body.onload = () => {
   const canvas = getCanvas();
   const context = getCanvasContext(canvas);
-  const { width, height } = canvas;
-  const center: IPointTuple = [width / 2, height / 2];
   const initialAngle = Math.random() * Math.PI * 2;
-  const initialVelocity = Vector.fromAngle(initialAngle).multiply(INITIAL_SPEED);
-  const particle = Particle.create({ position: Vector.fromTuple(center), velocity: initialVelocity });
+  const particle = Particle.create({
+    position: Vector.fromTuple([canvas.width / 2, canvas.height / 2]),
+    velocity: Vector.fromAngle(initialAngle).multiply(INITIAL_SPEED),
+  });
 
-  loop(render(context, { particle, width, height }), FPS);
+  loop(render(context, { particle }), FPS);
 };
 
 const render = (context: CanvasRenderingContext2D, data: ISketchData) => (t: number) => {
