a0f26c8c2e2338e042fff5c00059ec001ecd7b05
Daniel Imal <mrdanimal@gmail.com>
2025-05-09 12:04:57 -0400

    use Newtonian physics

--- sketches/particles/particles.ts
+++ sketches/particles/particles.ts
@@ -1,45 +1,57 @@
-import { type IParticle, create, applyForce } from './Particle.js';
-import { getCanvas, getCanvasContext, type IPointTuple, loop } from './particles.utils.js';
-import * as Vec from './Vector.js';
+import * as Vector from './Vector';
+import { type IParticle, create, applyForce } from './Particle';
+import { getCanvas, getCanvasContext, type IPointTuple, loop } from './particles.utils';
+import * as Vec from './Vector';
+
+const INITIAL_SPEED = 150;
 
 document.body.onload = () => {
   const canvas = getCanvas();
   const context = getCanvasContext(canvas);
   const { width, height } = canvas;
-  const center: IPointTuple = [width / 2, height / 2];
-  const initialVelocity = 150;
-  const angle = Math.random() * Math.PI * 2;
-  const initialForce = Vec.multiply(Vec.fromAngle(angle), initialVelocity);
-  const particle = create({ position: Vec.fromTuple(center), velocity: initialForce });
-  const force: Vec.IVector = { x: 0, y: 0 };
 
-  loop(render(context, { particle, force }), 60);
+  const center: IPointTuple = [width / 2, height / 2];
+  const angle = Math.random() * Math.PI * 2;
+  const initialForce = Vec.multiply(Vec.fromAngle(angle), INITIAL_SPEED);
+
+  const data = {
+    particle: create({ position: Vec.fromTuple(center), velocity: initialForce }),
+  };
+
+  const FPS = 60;
+
+  loop(render(context, data), FPS);
 };
 
 interface ISketchData {
   particle: IParticle;
-  force: Vec.IVector;
 }
 
 const render = (context: CanvasRenderingContext2D, data: ISketchData) => (t: number) => {
   const { width, height } = context.canvas;
+  const dt = 1 / 60;
 
   let particle = data.particle;
 
-  // Bounce off walls
+  let x = 0;
+  let y = 0;
+
+  // Compute impulse to reverse velocity component on collision
   if (particle.position.x < 0 || particle.position.x > width) {
-    particle = { ...particle, velocity: { ...particle.velocity, x: -particle.velocity.x } };
+    x = (-2 * particle.velocity.x * particle.mass) / dt;
   }
 
   if (particle.position.y < 0 || particle.position.y > height) {
-    particle = { ...particle, velocity: { ...particle.velocity, y: -particle.velocity.y } };
+    y = (-2 * particle.velocity.y * particle.mass) / dt;
   }
 
   particle = applyForce({
-    particle: particle,
-    force: data.force,
-    deltaTime: 1 / 60,
+    particle,
+    force: Vector.create(x, y),
+    deltaTime: dt,
   });
+
+  data.particle = particle;
 
   context.clearRect(0, 0, width, height);
 
