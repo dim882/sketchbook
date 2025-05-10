800719ebfa5f93e06ae0d6b5ac0dc4cdf43150a8
Daniel Imal <mrdanimal@gmail.com>
2025-05-10 19:46:40 -0400

    start to tighten up boids

--- sketches/boids/boids.ts
+++ sketches/boids/boids.ts
@@ -1,8 +1,8 @@
 import * as Boid from './Boid';
 import * as utils from './boids.utils';
 
-const BACKGROUND_COLOR = '#f5f5f5';
-const BOID_COUNT = 100;
+const BACKGROUND_COLOR = '#fcfaf7';
+const BOID_COUNT = 500;
 const SEPARATION_DISTANCE = 100;
 const ALIGNMENT_DISTANCE = 50;
 const COHESION_DISTANCE = 50;
@@ -32,19 +32,14 @@ window.addEventListener('DOMContentLoaded', () => {
   };
 
   let flock = utils.createFlock(BOID_COUNT, canvas.width, canvas.height, prng);
-
-  // Store the paths of each boid
   const boidPaths: { x: number; y: number }[][] = flock.map(() => []);
 
   function animate() {
+    if (!context) return;
+
     const { width, height } = canvas;
 
-    if (!context) {
-      return;
-    }
-
-    // Clear canvas
-    // context.fillStyle = BACKGROUND_COLOR;
+    context.fillStyle = BACKGROUND_COLOR;
     context.clearRect(0, 0, width, height);
 
     // Update and draw boids
@@ -52,24 +47,22 @@ window.addEventListener('DOMContentLoaded', () => {
       let newBoid = utils.flock(boid, flock, flockParams, width, height);
 
       newBoid = Boid.update(newBoid);
-
       newBoid = Boid.wrap(newBoid, width, height);
 
-      // Store the boid's position in its path
-      boidPaths[index].push({ x: newBoid.position.x, y: newBoid.position.y });
+      boidPaths[index].push(newBoid.position);
 
       // Limit the path length
       if (boidPaths[index].length > PATH_LENGTH_LIMIT) {
-        boidPaths[index].shift(); // Remove the oldest point
+        boidPaths[index].shift();
       }
 
-      // Draw the boid's path
       context.beginPath();
       context.strokeStyle = newBoid.color;
       context.lineWidth = 0.5;
 
       if (boidPaths[index].length > 1) {
         context.moveTo(boidPaths[index][0].x, boidPaths[index][0].y);
+
         for (let i = 1; i < boidPaths[index].length; i++) {
           context.lineTo(boidPaths[index][i].x, boidPaths[index][i].y);
         }
