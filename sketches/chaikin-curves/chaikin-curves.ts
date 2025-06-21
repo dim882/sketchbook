9a225d865462242b9f93933a0ce57d7a74fd606d
Daniel Imal <mrdanimal@gmail.com>
2025-06-21 04:29:42 -0400

    try drawing a white outline

--- sketches/chaikin-curves/chaikin-curves.ts
+++ sketches/chaikin-curves/chaikin-curves.ts
@@ -28,16 +28,28 @@ const sketch = () => {
   const smoothPath = utils.applyChaikinCurve(path, CHAIKIN_ITERATIONS);
 
   if (smoothPath.length > 0) {
-    context.strokeStyle = LINE_COLOR;
-    context.lineWidth = LINE_WIDTH;
-    context.beginPath();
-    context.moveTo(smoothPath[0].x, smoothPath[0].y);
+    context.lineCap = 'round';
 
     for (let i = 1; i < smoothPath.length; i++) {
-      context.lineTo(smoothPath[i].x, smoothPath[i].y);
+      drawLine(context, smoothPath, i, BACKGROUND_COLOR, 15);
+      drawLine(context, smoothPath, i, LINE_COLOR, LINE_WIDTH);
     }
-    context.stroke();
   }
 };
 
 window.addEventListener('load', sketch);
+
+function drawLine(
+  context: CanvasRenderingContext2D,
+  smoothPath: utils.IPoint[],
+  i: number,
+  strokeStyle: string | CanvasGradient | CanvasPattern,
+  lineWidth: number
+) {
+  context.strokeStyle = strokeStyle;
+  context.lineWidth = lineWidth;
+  context.beginPath();
+  context.moveTo(smoothPath[i - 1].x, smoothPath[i - 1].y);
+  context.lineTo(smoothPath[i].x, smoothPath[i].y);
+  context.stroke();
+}
