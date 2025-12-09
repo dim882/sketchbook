dcc589b574edb4490fe67b71b58ff39f6115c34b
Daniel Imal <mrdanimal@gmail.com>
2025-12-09 17:28:14 -0500

    add background

--- sketches/blob-path/src/blob-path.ts
+++ sketches/blob-path/src/blob-path.ts
@@ -38,7 +38,8 @@ function render(context: CanvasRenderingContext2D, rand: PseudoRandomNumberGener
   const { width, height } = context.canvas;
   const center: IPointTuple = [width / 2, height / 2];
 
-  context.clearRect(0, 0, width, height);
+  context.fillStyle = '#fcfaf7';
+  context.fillRect(0, 0, width, height);
 
   const point1: IPointTuple = getRandomEdgePoint(rand, width, height);
   const point2: IPointTuple = getRandomEdgePoint(rand, width, height);
